e5f27ba6801636a8fac30ee806a506ffd994b1a7
Nicholas Graumann <ravotus@gmail.com>
2016-11-12 16:26:24 -0600

    Add script to revert things after generation

---
+++ FaucetLED/reset_after_generation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script (roughly) resets the project after generating new
+# code via CubeMX. Specifically, FreeRTOS is custom (9.0.0 vs 8.x), and
+# the .cproject file needs some xml fixups to avoid lots of diff noise.
+
+git checkout -- Middlewares/Third_Party/FreeRTOS/Source
+sed -i -b 's_ />_/>_' .cproject
+sed -i -b 's_<?xml version="1.0" encoding="UTF-8"?>_<?xml version="1.0" encoding="UTF-8" standalone="no"?>_' .cproject
+
